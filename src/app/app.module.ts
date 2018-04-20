2f5cbb7fc3a8e49bf46120b3dd6ff57f2d6eb1e0
cmtp <christian.tola@hotmail.com>
2018-04-20 19:55:19 -0400

    add new component catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 import 'hammerjs';
 import { CatalogComponent } from './catalog/catalog.component';
@@ -24,7 +26,9 @@ import { CatalogComponent } from './catalog/catalog.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
