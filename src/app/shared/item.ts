b3810d4d71412de2c1c48462fe230f8b461ef826
cmtp <christian.tola@hotmail.com>
2018-04-20 20:27:21 -0400

    add Binding data

---
+++ src/app/shared/item.ts
@@ -0,0 +1,11 @@
+import { Comment } from './comment';
+
+export class Item {
+  name: string;
+  image: string;
+  category: string;
+  label: string;
+  price: string;
+  description: string;
+  comments: Comment[];
+}
