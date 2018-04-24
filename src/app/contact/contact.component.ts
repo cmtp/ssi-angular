6b6a6280d6dcf032d8279cc2b7b8c79a82828239
cmtp <christian.tola@hotmail.com>
2018-04-23 20:20:47 -0400

    add reactive form with validations

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -38,8 +38,20 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+
+    var form: HTMLFormElement =
+      <HTMLFormElement>document.getElementById('form');
+    form.reset();
+
   }
 
-
-}
+  }
